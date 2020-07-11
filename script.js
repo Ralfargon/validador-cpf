67b8deffde8b763ee5aaabe1da136b69efd3defd
Ralfargon <62768635+Ralfargon@users.noreply.github.com>
2020-07-11 12:28:31 -0300

    update script
    update script

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
 console.log("JavaScript Carregado!");
 
 function validaCPF(cpf) {
-    console.log (cpf.length)
-    if(cpf.length != 11){
+    console.log(cpf.length)
+    if (cpf.length != 11) {
         return false;
     }
     else {
-       return true;
+
+        var numeros = cpf.substring(0, 9);
+        var digitos = cpf.substring(9);
+
+        var soma = 0;
+
+        for (var i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+        }
+
+        var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+        if (resultado != digitos.charAt(0)) {
+            return false;
+        }
+
+        //////////////////////////
+
+        soma = 0;
+        numeros = cpf.substring(0, 10);
+
+        for (var k = 11; k > 1; k--) {
+            soma += numeros.charAt(11 - k) * k;
+        }
+
+        var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+        if (resultado != digitos.charAt(1)) {
+            return false;
+        }
+
+        return true;
     }
 }
 
+
+
+
 function validacao() {
     console.log("Iniciando");
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
+    
+
     var cpf = document.getElementById('cpf_digitado').value;
 
     var resultadoValidacao = validaCPF(cpf);
@@ -23,5 +59,3 @@ function validacao() {
         document.getElementById('error').style.display = 'block';
     }
 }
-
-
